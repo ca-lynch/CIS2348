0bb0353dcbc1337502b1210d3b20a03dff891590
Cody Lynch <70176681+ca-lynch@users.noreply.github.com>
2021-01-21 17:24:29 -0600

    Add files via upload

--- zybooks 2.19.py
+++ zybooks 2.19.py
@@ -1,20 +1,38 @@
 # Cody Lynch
 # 1954220
 
-paint_colors = {
-   'red': 35,
-   'blue': 25,
-   'green': 23
-}
+# Gets initial list of ingredients and amounts
+lemon_juice_cups = float(input('Enter amount of lemon juice (in cups):\n'))
+water_cups = float(input("Enter amount of water (in cups):\n"))
+agave_cups = float(input("Enter amount of agave nectar (in cups):\n"))
+servings = float(input("How many servings does this make?\n"))
 
-wall_height = int(input('Enter wall height (feet):\n'))
-wall_width = int(input('Enter wall width (feet):\n'))
-wall_area = wall_height * wall_width
-print('Wall area: {} square feet'.format(wall_area))
-paint_needed = wall_area / 350
-print("Paint needed: {:.2f} gallons".format(paint_needed))
-cans_needed = round(paint_needed)
-print("Cans needed: {} can(s)".format(cans_needed))
+# Outputs ingredients and amounts
+print("\nLemonade ingredients - yields", "{:.2f}".format(servings), "servings")
+print("{:.2f}".format(lemon_juice_cups), "cup(s) lemon juice")
+print("{:.2f}".format(water_cups), "cup(s) water")
+print("{:.2f}".format(agave_cups), "cup(s) agave nectar\n")
 
-desired_color = (input("\nChoose a color to paint the wall:"))
-print("\nCost of purchasing {} paint: ${}".format(desired_color, paint_colors[desired_color] * cans_needed))
+# Gets users desired amount of lemonade, and calculates amount of ingredients needed
+desired_amount = float(input("How many servings would you like to make?\n"))
+adjustment_number = desired_amount / servings
+adjusted_lemon = lemon_juice_cups * adjustment_number
+adjusted_water = water_cups * adjustment_number
+adjusted_agave = agave_cups * adjustment_number
+
+# Outputs amount of ingredients needed to make desired amount in cups
+print("\nLemonade ingredients - yields", "{:.2f}".format(desired_amount), "servings")
+print("{:.2f}".format(adjusted_lemon), "cup(s) lemon juice")
+print("{:.2f}".format(adjusted_water), "cup(s) water")
+print("{:.2f}".format(adjusted_agave), "cup(s) agave nectar\n")
+
+# Converts ingredients amount from cups to gallons
+lemon_gallon = adjusted_lemon / 16
+water_gallon = adjusted_water / 16
+agave_gallon = adjusted_agave / 16
+
+# Outputs amount of ingredients needed in gallons
+print("Lemonade ingredients - yields", "{:.2f}".format(desired_amount), "servings")
+print("{:.2f}".format(lemon_gallon), "gallon(s) lemon juice")
+print("{:.2f}".format(water_gallon), "gallon(s) water")
+print("{:.2f}".format(agave_gallon), "gallon(s) agave nectar")
