3e4f3b82e3cd00cd424951040018e3d4dfb48c47
Cody Lynch <70176681+ca-lynch@users.noreply.github.com>
2021-01-27 15:00:04 -0600

    Delete zybooks 1.12.py

--- zybooks 1.12.py
+++
@@ -1,9 +0,0 @@
-# Cody Lynch
-# 1954220
-
-# Gets an integer from user
-userNum = int(input())
-# Calculates users integer squared
-userNumSquared = userNum * userNum
-# Outputs users integer squared
-print(userNumSquared)
